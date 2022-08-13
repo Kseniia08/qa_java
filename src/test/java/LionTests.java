5a92cf3b947e161d6a57a9592ddf52508df85e99
Kseniia Erminskaia <erminskayaxenia@yandex.ru>
2022-08-13 13:43:29 +0300

    fix for mocks in LionTests.java

--- src/test/java/LionTests.java
+++ src/test/java/LionTests.java
@@ -12,40 +12,54 @@ import static org.junit.Assert.assertEquals;
 @RunWith(MockitoJUnitRunner.class)
 public class LionTests {
 
+    @Mock
     Feline feline = new Feline();
-
-    //Тест на проверку списка еды для Хищника (семейство кошачьих - хищники)
+    //Тест с заглушкой
     @Test
-    public void eatMeatReturnsListOfPredatorFoodForLionTest() throws Exception {
-        List<String> expected = List.of("Животные", "Птицы", "Рыба");
-        Lion lion = new Lion("Самка", feline);
-        List <String> actual = lion.getFood();
-        assertEquals("eatMeatList is correct",expected, actual);
+    public void getKittensForLionTests() throws Exception {
+        Lion lion = new Lion("Самец", feline);
+        Mockito.when(feline.getKittens()).thenReturn(1);
+        int actualGetKittens = lion.getKittens();
+        assertEquals("getKittens with mock returns default int 1",1, actualGetKittens);
     }
 
-    //Тест на проверку, что список еды Хищника включает в себя Рыбу
+    //Тест на проверку списка еды для Хищника (семейство кошачьих - хищники) c заглушкой
+    @Test
+    public void eatMeatReturnsListOfPredatorFoodForLionTest() throws Exception {
+        Lion lion = new Lion("Самка", feline);
+        Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        System.out.println(feline.getFood("Хищник"));
+        List <String> actual = lion.getFood();
+        assertEquals("eatMeatList is correct",List.of("Животные", "Птицы", "Рыба"), actual);
+    }
+
+
+    //Тест на проверку, что список еды Хищника включает в себя Рыбу с заглушкой
     @Test
     public void eatMeatContainsFishInListOfPredatorFoodForLionTest() throws Exception {
         String fish = "Рыба";
         Lion lion = new Lion("Самка", feline);
+        Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
         boolean actual = lion.getFood().contains(fish);
         Assert.assertTrue("Fish is a part of getFood", actual);
     }
 
-    //Тест на проверку, что список еды Хищника включает в себя Птиц
+    //Тест на проверку, что список еды Хищника включает в себя Птиц с заглушкой
     @Test
     public void eatMeatContainsBirdsInListOfPredatorFoodForLionTest() throws Exception {
         String birds = "Птицы";
         Lion lion = new Lion("Самка", feline);
+        Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
         boolean actual = lion.getFood().contains(birds);
         Assert.assertTrue("Birds are a part of getFood",actual);
     }
 
-    //Тест на проверку, что список еды Хищника включает в себя Животных
+    //Тест на проверку, что список еды Хищника включает в себя Животных с заглушкой
     @Test
     public void eatMeatContainsAnySmallAnimalsInListOfPredatorFoodTest() throws Exception {
         String anySmallAnimals = "Птицы";
         Lion lion = new Lion("Самка", feline);
+        Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
         boolean actual = lion.getFood().contains(anySmallAnimals);
         Assert.assertTrue("Any small animals are a part of eatMeatList",actual);
     }
@@ -77,16 +91,5 @@ public class LionTests {
         boolean actualSex = lion.doesHaveMane();
         Assert.assertTrue( "The lion is a male" , actualSex);
     }
-
-    //Тест с заглушкой
-    @Mock
-    Feline anotherFeline;
-    @Test
-    public void getKittensForLionTests() throws Exception {
-        Lion lion = new Lion("Самец", anotherFeline);
-        Mockito.when(anotherFeline.getKittens()).thenReturn(1);
-        int actualGetKittens = lion.getKittens();
-        assertEquals("getKittens with mock returns default int 1",1, actualGetKittens);
-        }
 
 }
